98667562bbefdda74f02574003441fef4feea4f2
unknown <zab_vl@tutby>
2023-03-14 08:45:55 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+int number =Convert.ToInt32(Console.ReadLine());
+
+if(number/100!=0&&number/1000==0)
+{
+    Console.WriteLine($"Вторая цифра равна {(number%100)/10}");
+}
+else
+{
+    Console.WriteLine("число не является трехзначным!");
+}
